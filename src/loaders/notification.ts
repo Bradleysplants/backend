3f0f0ffb4266a2ee58611d580d38286c444fd338
Bradley Lawson-Ford <149356145+Bradleysplants@users.noreply.github.com>
2024-02-08 11:50:40 -0600

    Update notification.ts

--- src/loaders/notification.ts
+++ src/loaders/notification.ts
@@ -1,4 +1,5 @@
 import { AwilixContainer } from "awilix";
+import EmailSenderService from "../services/email-sender";
 
 export default async (container: AwilixContainer): Promise<void> => {
   const notificationService = container.resolve("notificationService");
