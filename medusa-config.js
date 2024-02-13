9d0df14e4766964dca74461474f33eeb00706cc6
Bradley Lawson-Ford <149356145+Bradleysplants@users.noreply.github.com>
2024-02-12 18:22:15 -0600

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -24,14 +24,14 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = process.env.ADMIN_CORS;
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS;
 
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+const DATABASE_URL = process.env.DATABASE_URL;
 
-const REDIS_URL = process.env.REDIS_URL || "redis://default:jaJcjndJKh34BNbkeogoNPaifmGlDN1B@roundhouse.proxy.rlwy.net:27355";
+const REDIS_URL = process.env.REDIS_URL;
 
 const plugins = [
   `medusa-fulfillment-manual`,
