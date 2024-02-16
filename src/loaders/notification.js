44a956b88c2470a8e0f6f266675aaf199a12d262
Bradley Lawson-Ford <149356145+Bradleysplants@users.noreply.github.com>
2024-02-15 22:11:05 -0600

    commit

--- src/loaders/notification.js
+++
@@ -1,6 +0,0 @@
-const EmailSenderService = require("../path/to/email-sender");
-
-module.exports = async (container) => {
-  const notificationService = container.resolve("notificationService");
-  notificationService.subscribe("order.placed", EmailSenderService.identifier);
-};
